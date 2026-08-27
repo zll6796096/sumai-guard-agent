5b998b9e038e18dcca80ad6a63347db067c34efa
ZHANG LONGLONG <zll6796096@gmail.com>
2026-08-28 00:32:55 +0900

    test(ios): update build 2 contract

--- ios/SumaiGuardTests/ProjectContractTests.swift
+++ ios/SumaiGuardTests/ProjectContractTests.swift
@@ -14,9 +14,9 @@ final class ProjectContractTests: XCTestCase {
         XCTAssertEqual(appInfo["CFBundleDisplayName"] as? String, "実家チェック")
     }
 
-    func testAppStartsAtVersionOneBuildOne() {
+    func testAppStartsAtVersionOneBuildTwo() {
         XCTAssertEqual(appInfo["CFBundleShortVersionString"] as? String, "1.0")
-        XCTAssertEqual(appInfo["CFBundleVersion"] as? String, "1")
+        XCTAssertEqual(appInfo["CFBundleVersion"] as? String, "2")
     }
 
     func testAppTargetsIPhoneOnIOS17OrLater() {
